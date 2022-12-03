523b0de4b243ea8bf1eaa10149edd9879f5d78f1
Sebastian <foerster@forestsoft.de>
2022-12-03 21:49:05 +0100

    fix: check if there is an ssh service
    we should check if there is an ssh and database service before we are
    trying to port forward

--- main.go
+++ main.go
@@ -5,40 +5,50 @@ import (
 	"fmt"
 
 	"github.com/ForestsoftGmbH/devctl/client"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
+
+var kubeClient *kubernetes.Clientset
+var workingNamespace string
+var config *rest.Config
+var serviceCollection *client.ServiceCollection
 
 func main() {
 
 	remotePort := flag.Int("p", 0, "Port for remote connection")
-	kubeClient, config, workingNamespace := client.New()
+	kubeClient, config, workingNamespace = client.New()
 
 	fmt.Println("Working namespace:", workingNamespace)
 
-	var service *client.Service
-
-	serviceCollection := &client.ServiceCollection{}
+	serviceCollection = &client.ServiceCollection{}
+	services := make([]client.Service, 0)
+	serviceCollection.Services = services
 
 	if *remotePort == 22 {
-		service = client.GetSSHService(kubeClient, workingNamespace)
-		services := []client.Service{
-			*service,
-		}
-		serviceCollection.Services = services
+		addSSHService()
 	} else if *remotePort == 3306 {
-		service = client.GetDatabaseService(kubeClient, workingNamespace)
-		services := []client.Service{
-			*service,
-		}
-		serviceCollection.Services = services
+		addDatabaseService()
 	} else {
-		sshService := client.GetSSHService(kubeClient, workingNamespace)
-		databaseService := client.GetDatabaseService(kubeClient, workingNamespace)
-		services := []client.Service{
-			*sshService,
-			*databaseService,
-		}
-		serviceCollection.Services = services
+		addSSHService()
+		addDatabaseService()
 	}
-
 	client.Forward(serviceCollection, config)
+}
+
+func addDatabaseService() {
+	databaseService := client.GetDatabaseService(kubeClient, workingNamespace)
+	if databaseService.Port > 0 {
+		serviceCollection.Services = append(serviceCollection.Services, *databaseService)
+	} else {
+		fmt.Println("No database service found")
+	}
+}
+func addSSHService() {
+	sshService := client.GetSSHService(kubeClient, workingNamespace)
+	if sshService.Port > 0 {
+		serviceCollection.Services = append(serviceCollection.Services, *sshService)
+	} else {
+		fmt.Println("No ssh service found")
+	}
 }
